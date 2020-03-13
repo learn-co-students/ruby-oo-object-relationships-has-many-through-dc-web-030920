bd3a1c40938a8d0dc0a2eee6b647a5c18caf84e9
aduterte <alexduterte@gmail.com>
2020-03-12 23:31:57 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
 class Waiter
 
+    attr_reader :name, :num
+    @@all = []
+    def initialize(name, num)
+        @name = name
+        @experience = num
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|x| x.waiter == self}
+    end
+
+    def best_tipper
+        # binding.pry
+        
+        
+        meals.max do |x, y| 
+        #    binding.pry
+          x.tip <=> y.tip
+        end.customer
+      
+        #    puts instance.customer.tip
+        # end
+        # # binding.pry
+    end
 end
