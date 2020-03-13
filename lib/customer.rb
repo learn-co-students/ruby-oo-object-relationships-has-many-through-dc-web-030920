162d3a4fa90b1baf9f20f2ceca45bd80ee0b81d2
kaidornel <kai_dornel@hotmail.com>
2020-03-12 23:38:14 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Customer
-  
+    attr_reader :name, :age, :waiter
+    @@all = []
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal_instance|
+            meal_instance.customer == self
+        end
+    end
+
+    def waiters
+        meals.map do |mealcontainingcustomerwaiter_instance|
+            mealcontainingcustomerwaiter_instance.waiter
+        end
+    end
+
+
 end
+
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
 class Waiter
+    attr_reader :name, :years, :meal
+    @@all = []
+    def initialize(name, years)
+        @name = name
+        @meal = meal
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal_instance|
+            meal_instance.waiter == self
+        end
+    end
+
+    def best_tipper
+        variable = meals.max do |meal_instance, meal_instance2|
+            meal_instance.tip <=> meal_instance2.tip
+        end
+        variable.customer
+    end
 
 end
